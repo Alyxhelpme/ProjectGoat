48d27d914f65248d80e521a5e3ceaec8c8ed3d29
Alyxhelpme <74560600+Alyxhelpme@users.noreply.github.com>
2022-03-02 15:04:11 -0600

    Game now restarts after .5f

--- NotGoatSimulator/Assets/Script/Personaje.cs
+++ NotGoatSimulator/Assets/Script/Personaje.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI; 
-using  UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Personaje : MonoBehaviour {
     [SerializeField]
     private Rigidbody rb;
-    
+    public float delay=3; //El tiempo de delay para el cambio de escena
     
     public float velocidad=70;
     void Awake() { print("EJECUTANDO");}
@@ -16,8 +16,6 @@ public class Personaje : MonoBehaviour {
         Debug.Log("START");
         rb=GetComponent<Rigidbody>();
         rb.freezeRotation=true;
-        
-
     }
     void Update() {
         rb.AddForce(new Vector3(.7f,0,0));
@@ -36,15 +34,23 @@ public class Personaje : MonoBehaviour {
 
     }
 
+    // we are using a Couroutine declaration. Execution of couroutine is paused when yield statement is called
+    //  IEnumerator is used to declare the declaration itself, using a "return yield" pauses the execution of the method
+    // So, in this case as we want the scene to change after a few seconds we use yield command with WaitForSeconds. 
+
+    IEnumerator reset(){
+        yield return new WaitForSeconds(.5f);
+        SceneManager.LoadScene("Game");
+    }
+
     void OnCollisionEnter(Collision c){ //Collision tiene info detallada de la colision 
         UnityEngine.Debug.Log("goatCrash");
         print("onCollisionStart" + c.contacts[0]);
         print(c.transform.name);
         if(c.transform.name=="Cube"){
         //     rb.velocity=Vector3.zero;
-            SceneManager.LoadScene("Game");
+            StartCoroutine(reset());
         }
-        
 
     }
     void OnCollisionStay(Collision c){
